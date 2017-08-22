402974854a6fcd0d23684447949254a90d9f9ca5
yukisora <yukisora2013@gmail.com>
2017-08-22 15:34:29 +0800

    swipe right to close

--- app/src/main/java/moe/yukisora/solidot/activities/ArticleActivity.java
+++ app/src/main/java/moe/yukisora/solidot/activities/ArticleActivity.java
@@ -3,22 +3,29 @@ package moe.yukisora.solidot.activities;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
+import android.view.GestureDetector;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.widget.TextView;
 
 import moe.yukisora.solidot.R;
 import moe.yukisora.solidot.modles.ArticleData;
 
 public class ArticleActivity extends AppCompatActivity {
+    private GestureDetectorCompat detector;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article);
         ArticleData article = (ArticleData)getIntent().getSerializableExtra("articleData");
+
+        detector = new GestureDetectorCompat(this, new GestureListener());
 
         Toolbar toolbar = findViewById(R.id.articleActivityToolbar);
         TextView title = findViewById(R.id.articleActivityTitle);
@@ -56,4 +63,37 @@ public class ArticleActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent e) {
+        super.dispatchTouchEvent(e);
+
+        return detector.onTouchEvent(e);
+    }
+
+    private void onSwipeLeft() {
+    }
+
+    private void onSwipeRight() {
+        finish();
+    }
+
+    private class GestureListener extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            float diffX = e2.getX() - e1.getX();
+            float diffY = e2.getY() - e1.getY();
+            if (Math.abs(diffX) > Math.abs(diffY) && Math.abs(diffX) > 100 && Math.abs(velocityX) > 100) {
+                if (diffX > 0) {
+                    onSwipeRight();
+                } else {
+                    onSwipeLeft();
+                }
+                return true;
+            }
+
+            return false;
+        }
+    }
+
 }
