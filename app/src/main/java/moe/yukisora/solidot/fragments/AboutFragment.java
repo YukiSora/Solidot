542a660228771279f521bd641a7ede55567f7c6e
yukisora <yukisora2013@gmail.com>
2017-08-24 21:39:55 +0800

    license

--- app/src/main/java/moe/yukisora/solidot/fragments/AboutFragment.java
+++ app/src/main/java/moe/yukisora/solidot/fragments/AboutFragment.java
@@ -1,7 +1,11 @@
 package moe.yukisora.solidot.fragments;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.text.Html;
+import android.widget.TextView;
 
 import moe.yukisora.solidot.R;
 
@@ -18,5 +22,19 @@ public class AboutFragment extends PreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences_about);
+
+        findPreference("license").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle(getResources().getString(R.string.license));
+                builder.setMessage(Html.fromHtml(getResources().getString(R.string.licenses)));
+                AlertDialog alert = builder.create();
+                alert.show();
+                ((TextView)alert.findViewById(android.R.id.message)).setTextSize(10);
+
+                return true;
+            }
+        });
     }
 }
